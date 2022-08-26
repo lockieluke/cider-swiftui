a404d6ca2e1fe06d01b711b15591ad49b482d289
Sherlock Luk <sherlockluk@outlook.com>
2022-08-26 16:40:37 +0100

    Initial Commit

---
+++ Cider/CiderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CiderApp.swift
+//  Cider
+//
+//  Created by Sherlock LUK on 26/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct CiderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
