c4f5a280822f42d443ef4e452c988daa4d3dc477
Sherlock Luk <sherlockluk@outlook.com>
2022-10-22 21:43:27 +0100

    new animations and add play button

--- Cider/Views/AMPresentable.swift
+++ Cider/Views/AMPresentable.swift
@@ -12,6 +12,10 @@ struct AMPresentable: View {
     public var recommendation: AMMediaItem
     private let PRESENTABLE_IMG_SIZE = CGSize(width: 200, height: 200)
     
+    @State private var isHovering = false
+    @State private var isHoveringPlay = false
+    @State private var isClicked = false
+    
     var body: some View {
         VStack {
             AsyncImage(url: URL(string: recommendation.artwork.url.replacingOccurrences(of: "{w}", with: "200").replacingOccurrences(of: "{h}", with: "200"))) { image in
@@ -19,9 +23,52 @@ struct AMPresentable: View {
                     .resizable()
                     .scaledToFit()
                     .frame(width: PRESENTABLE_IMG_SIZE.width, height: PRESENTABLE_IMG_SIZE.height)
+                    .cornerRadius(5)
+                    .brightness(isHovering ? (isClicked ? -0.15 : -0.1) : 0)
+                    .animation(.easeIn(duration: 0.1), value: isHovering)
             } placeholder: {
                 ProgressView()
             }
+            .overlay {
+                if isHovering {
+                    HStack {
+                        VStack {
+                            let background = RoundedRectangle(cornerRadius: 20, style: .continuous)
+                            
+                            Spacer()
+                            HStack {
+                                Image(systemName: "play.fill")
+                                    .font(.system(size: 13, weight: .bold, design: .rounded))
+                                    .foregroundColor(isHoveringPlay ? .pink : .primary)
+                                Text("Play")
+                            }
+                                .font(.system(.body, design: .rounded))
+                                .foregroundColor(.primary)
+                                .padding(.horizontal, 10)
+                                .padding(.vertical, 5)
+                                .background(.ultraThinMaterial, in: background)
+                                .contentShape(background)
+                                .onHover { isHovering in
+                                    self.isHoveringPlay = isHovering
+                                }
+                        }
+                        .padding(.bottom, 10)
+                        Spacer()
+                    }
+                    .padding(.leading, 10)
+                    .transition(.opacity)
+                }
+            }
+            .onHover { isHovering in
+                withAnimation(.easeIn(duration: 0.15)) {
+                    self.isHovering = isHovering
+                }
+            }
+            .gesture(DragGesture(minimumDistance: 0).onChanged({ _ in
+                self.isClicked = true
+            }).onEnded({_ in
+                self.isClicked = false
+            }))
             Text("\(recommendation.title)")
         }
         .frame(width: PRESENTABLE_IMG_SIZE.width + 50, height: PRESENTABLE_IMG_SIZE.height + 50)
