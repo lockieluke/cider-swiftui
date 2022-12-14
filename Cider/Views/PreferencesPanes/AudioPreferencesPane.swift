af7bee29854a45288d1b24fb89d7d02fc6ef5704
Sherlock Luk <sherlockluk@outlook.com>
2022-12-14 20:21:42 +0000

    add audio quality descriptions

--- Cider/Views/PreferencesPanes/AudioPreferencesPane.swift
+++ Cider/Views/PreferencesPanes/AudioPreferencesPane.swift
@@ -13,13 +13,28 @@ struct AudioPreferencesPane: View {
     @EnvironmentObject private var prefModal: PrefModal
     @EnvironmentObject private var ciderPlayback: CiderPlayback
     
+    var audioQualityDescription: String {
+        get {
+            switch self.prefModal.prefs.audioQuality {
+                
+            case .Standard:
+                return "Standard Quality contains less detail of the audio but can help save network bandwidth"
+                
+                
+            case .High:
+                return "High Quality contains more detail of the audio but consumes more network bandwidth"
+                
+            }
+        }
+    }
+    
     var body: some View {
         Preferences.Container(contentWidth: 450.0) {
             Preferences.Section(title: "") {
                 Group {
                     PrefSectionText("Playback Settings")
                     
-                    HStack {
+                    VStack(alignment: .leading) {
                         Picker("Audio Quality", selection: $prefModal.prefs.audioQuality) {
                             Text("High 256kbps").tag(AudioQuality.High)
                             Text("Standard 64kbps").tag(AudioQuality.Standard)
@@ -29,8 +44,12 @@ struct AudioPreferencesPane: View {
                                 await self.ciderPlayback.setAudioQuality(audioQuality)
                             }
                         }
+                        
+                        Text(audioQualityDescription)
+                            .preferenceDescription()
                     }
                 }
+                .fixedSize(horizontal: false, vertical: true)
             }
         }
         .enableInjection()
