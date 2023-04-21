0d5098696f3fc741f59b24637099076d3da0ef3d
Sherlock Luk <sherlockluk@outlook.com>
2023-04-21 19:08:14 +0100

    use spinner to represent download progress

--- CiderDevUtils/src/functions/fetch-google-service.ts
+++ CiderDevUtils/src/functions/fetch-google-service.ts
@@ -1,6 +1,7 @@
 import * as https from "https";
 import * as fs from "fs";
 import * as path from "path";
+import ora from "ora";
 
 const cwd = process.env.CWD ?? process.cwd();
 const googleServicePath = path.join(cwd, 'Cider', 'GoogleService-Info.plist')
@@ -8,7 +9,7 @@ const exists = fs.existsSync(googleServicePath);
 if (exists)
     fs.unlinkSync(googleServicePath);
 
-console.log(`${exists ? "Refetching" : "Fetching"} GoogleService-Info.plist to ${googleServicePath}`);
+const spinner = ora(`${exists ? "Refetching" : "Fetching"} GoogleService-Info.plist to ${googleServicePath}`).start();
 const file = fs.createWriteStream(googleServicePath);
-https.get('https://firebasestorage.googleapis.com/v0/b/cider-collective.appspot.com/o/GoogleService-Info-macOS.plist?alt=media&token=191e51ff-51b6-4e72-9d60-fb63e2aa828d', res => res.pipe(file).on('finish', () => console.log("Done")))
-    .on('error', err => console.error(err));
+https.get('https://firebasestorage.googleapis.com/v0/b/cider-collective.appspot.com/o/GoogleService-Info-macOS.plist?alt=media&token=191e51ff-51b6-4e72-9d60-fb63e2aa828d', res => res.pipe(file).on('finish', () => spinner.succeed("Finished fetching GoogleService-Info.plist")))
+    .on('error', err => spinner.fail(err.message));
