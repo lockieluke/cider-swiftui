fe965cd8d600ffb8c59e7e1daca1ff9e317f8deb
Sherlock Luk <sherlockluk@outlook.com>
2023-07-22 22:01:24 +0100

    fix resizing and fade in animation

--- Cider/Views/MainViews/HomeView.swift
+++ Cider/Views/MainViews/HomeView.swift
@@ -22,58 +22,55 @@ struct HomeView: View {
     @ObservedObject private var iO = Inject.observer
     
     var body: some View {
-        ScrollView {
-            if !personalSocialProfile.isNil && dataLoaded, let personalSocialProfile = personalSocialProfile {
-                VStack {
-                    HStack {
-                        Text("Good \(DateUtils.timeOfDayInWords.lowercased()), \(personalSocialProfile.name)")
-                            .font(.title)
-                            .bold()
-                            .frame(maxWidth: .infinity, alignment: .leading)
-                            .padding(.vertical)
-                        Spacer()
-                        Button {
-                            let newIssueUrl = URL(string: "https://github.com/ciderapp/Cider-2/issues/new?assignees=&labels=\("🕓+Pending+Implementation,🐛+Bug".addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)!)&template=bug_report.yaml&title=\("[Bug] [macOS SwiftUI]:".addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)!)")
-                            
-                            newIssueUrl?.open()
-                        } label: {
-                            HStack {
-                                Image(systemName: "ladybug")
-                                Text("Report A Bug")
-                            }
-                        }
-                        .buttonStyle(.borderless)
-                    }
-                    PatchedGeometryReader { geometry in
-                        VStack(alignment: .leading, spacing: 20) {
-                            Text("Recently Played")
-                                .font(.title2.bold())
-                            MediaTableRepresentable(recentlyPlayedItems)
-                                .environmentObject(ciderPlayback)
-                            
-                            Text("Made For You")
-                                .font(.title2.bold())
-                            HStack {
-                                ForEach(personalRecommendation, id: \.id) { recommendationItem in
-                                    MediaPresentable(item: .mediaPlaylist(recommendationItem), maxRelative: geometry.maxRelative.clamped(to: 1000...1300), coverKind: "ss", geometryMatched: true)
-                                }
-                            }
-                        }
-                    }
+        ScrollView(.vertical) {
+            VStack {
+                HStack {
+                    Text("Good \(DateUtils.timeOfDayInWords.lowercased()), \(personalSocialProfile?.name ?? "")")
+                        .font(.title)
+                        .bold()
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding(.vertical)
                     Spacer()
+                    Button {
+                        let newIssueUrl = URL(string: "https://github.com/ciderapp/Cider-2/issues/new?assignees=&labels=\("🕓+Pending+Implementation,🐛+Bug".addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)!)&template=bug_report.yaml&title=\("[Bug] [macOS SwiftUI]:".addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)!)")
+                        
+                        newIssueUrl?.open()
+                    } label: {
+                        HStack {
+                            Image(systemName: "ladybug")
+                            Text("Report A Bug")
+                        }
+                    }
+                    .buttonStyle(.borderless)
                 }
-                .padding()
-                .transition(.opacity)
+                VStack(alignment: .leading, spacing: 20) {
+                    Text("Recently Played")
+                        .font(.title2.bold())
+                    MediaTableRepresentable(recentlyPlayedItems)
+                        .environmentObject(ciderPlayback)
+                    
+                    Text("Made For You")
+                        .font(.title2.bold())
+                    PatchedGeometryReader { geometry in
+                        HStack {
+                            ForEach(personalRecommendation, id: \.id) { recommendationItem in
+                                MediaPresentable(item: .mediaPlaylist(recommendationItem), maxRelative: geometry.maxRelative.clamped(to: 1000...1300), coverKind: "ss", geometryMatched: true)
+                            }
+                        }
+                    }
+                }
+                Spacer()
             }
+            .padding()
+            .opacity(dataLoaded ? 1 : 0)
+            .animation(.easeIn, value: dataLoaded)
         }
         .transparentScrollbars()
         .task {
             self.personalSocialProfile = await self.mkModal.AM_API.fetchPersonalSocialProfile()
             self.recentlyPlayedItems = await self.mkModal.AM_API.fetchRecentlyPlayed()
             self.personalRecommendation = await self.mkModal.AM_API.fetchPersonalRecommendation()
-            withAnimation(.spring()) {
-                self.dataLoaded = true
-            }
+            self.dataLoaded = true
         }
         .enableInjection()
     }
