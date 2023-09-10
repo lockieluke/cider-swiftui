3cf0a2834cd6294444eca541bd70375d0b37b991
Mono <79590499+Monochromish@users.noreply.github.com>
2023-09-11 04:52:21 +1000

    add hero card and display it's elements

--- Cider/API/MediaBrowseData.swift
+++ Cider/API/MediaBrowseData.swift
@@ -9,16 +9,82 @@
 import Foundation
 import SwiftyJSON
 
-struct MediaBrowseData {
-    
+struct EditorialKind: Hashable, Equatable {
+    let rawValue: String
+    static let unknown = EditorialKind(rawValue: "unknown")
+}
+
+struct BrowseItemAttributes: Hashable {
+    let designBadge: String
     let name: String
     let id: String
+    let kind: String
+    let artistName: String
+    let url: String
+    let artistUrl: String
+    let subscriptionHero: String
+    let plainEditorialNotes: String
+}
+
+struct MediaBrowseData {
+    
+    let id: String
+    let items: [BrowseItemAttributes]
+    let kind: EditorialKind
     
     init(data: JSON) {
-        let attributes = data["attributes"]
+        self.id = data["id"].stringValue
+        self.kind = EditorialKind(rawValue: data["attributes"]["editorialElementKind"].string ?? "unknown")
         
-        self.id = attributes["id"].stringValue
-        self.name = attributes["name"].stringValue
+        self.items = data["relationships"]["children"]["data"].array?.compactMap { child -> BrowseItemAttributes? in
+            let badge = child["attributes"]["designBadge"].string ?? ""
+            let meta = child["relationships"]["contents"]["data"].array?.first?["attributes"]
+            
+            guard let meta = meta else { return nil }
+                        
+            let name = meta["name"].string ?? meta["designTag"].string ?? ""
+            let artist = meta["curatorName"].string ?? meta["designTag"].string ?? meta["artistName"].string ?? ""
+            
+            let id = meta["playParams"]["id"].string ?? ""
+            let kind = meta["playParams"]["kind"].string ?? ""
+            
+            let url = meta["url"].string ?? ""
+            let artistUrl = meta["artistUrl"].string ?? ""
+            
+            // we're following my hierarchy :)
+            
+            var subscriptionHero: String
+            
+            if let url = meta["editorialArtwork"]["subscriptionHero"]["url"].string, !url.isEmpty {
+                let width = meta["editorialArtwork"]["subscriptionHero"]["width"].intValue
+                let height = meta["editorialArtwork"]["subscriptionHero"]["height"].intValue
+                subscriptionHero = url.replacingOccurrences(of: "{w}", with: "\(width)")
+                    .replacingOccurrences(of: "{h}", with: "\(height)")
+                    .replacingOccurrences(of: "{f}", with: "jpg")
+            } else if let url = meta["editorialArtwork"]["emailFeature"]["url"].string, !url.isEmpty {
+                let width = meta["editorialArtwork"]["emailFeature"]["width"].intValue
+                let height = meta["editorialArtwork"]["emailFeature"]["height"].intValue
+                subscriptionHero = url.replacingOccurrences(of: "{w}", with: "\(width)")
+                    .replacingOccurrences(of: "{h}", with: "\(height)")
+                    .replacingOccurrences(of: "{f}", with: "jpg")
+            } else if let url = meta["editorialArtwork"]["subscriptionCover"]["url"].string, !url.isEmpty {
+                let width = meta["editorialArtwork"]["subscriptionCover"]["width"].intValue
+                let height = meta["editorialArtwork"]["subscriptionCover"]["height"].intValue
+                subscriptionHero = url.replacingOccurrences(of: "{w}", with: "\(width)")
+                    .replacingOccurrences(of: "{h}", with: "\(height)")
+                    .replacingOccurrences(of: "{f}", with: "jpg")
+            } else {
+                let url = meta["artwork"]["url"].string ?? ""
+                let width = meta["artwork"]["width"].intValue
+                let height = meta["artwork"]["height"].intValue
+                subscriptionHero = url.replacingOccurrences(of: "{w}", with: "\(width)")
+                    .replacingOccurrences(of: "{h}", with: "\(height)")
+                    .replacingOccurrences(of: "{f}", with: "jpg")
+            }
+            
+            let plainEditorialNotes = meta["plainEditorialNotes"]["short"].string ?? ""
+            
+            return BrowseItemAttributes(designBadge: badge, name: name, id: id, kind: kind, artistName: artist, url: url, artistUrl: artistUrl, subscriptionHero: subscriptionHero, plainEditorialNotes: plainEditorialNotes)
+        } ?? []
     }
-    
 }
--- Cider/Views/MainViews/BrowseView.swift
+++ Cider/Views/MainViews/BrowseView.swift
@@ -16,6 +16,12 @@ struct BrowseView: View {
     @EnvironmentObject private var mkModal: MKModal
     
     @State private var browseData: [MediaBrowseData] = []
+    @State private var browseData316: [MediaBrowseData] = []
+    
+    @Namespace private var animationNamespace
+    
+    let heroCardSize: CGSize = CGSize(width: 550, height: 225)
+    let coverKindValue: String = "bb"
     
     var body: some View {
         ScrollView(.vertical) {
@@ -25,14 +31,29 @@ struct BrowseView: View {
                     .frame(maxWidth: .infinity, alignment: .leading)
                     .padding()
                 
-                ForEach(browseData, id: \.id) { browseDataRow in
-                    Text(browseDataRow.name)
+                ScrollView(.horizontal) {
+                    LazyHStack {
+                        ForEach(browseData316, id: \.id) { browseDataRow in
+                            ForEach(browseDataRow.items, id: \.self) { item in
+                                HeroCard(
+                                    item: item,
+                                    geometryMatching: animationNamespace,
+                                    originalSize: heroCardSize,
+                                    coverKind: coverKindValue
+                                )
+                                .frame(width: heroCardSize.width, height: heroCardSize.height)
+                            }
+                        }
+                    }
                 }
+                
+                
+                
             }
         }
-        .transparentScrollbars()
         .task {
             self.browseData = await self.mkModal.AM_API.fetchBrowse()
+            self.browseData316 = browseData.filter { $0.kind.rawValue == "316" }
         }
         .enableInjection()
     }
