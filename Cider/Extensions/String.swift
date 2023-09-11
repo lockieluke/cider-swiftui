e4c8319ad65c313df8fe7c0ede9a26ca2ad33e41
Sherlock Luk <sherlockluk@outlook.com>
2023-09-11 20:46:15 +0100

    fix compilation and move string extenions to the right place

--- Cider/Extensions/String.swift
+++ Cider/Extensions/String.swift
@@ -50,4 +50,13 @@ extension String {
         strings.contains { contains($0) }
     }
     
+    var htmlToAttributedString: NSAttributedString? {
+        guard let data = data(using: .utf8) else { return nil }
+        return try? NSAttributedString(data: data, options: [.documentType: NSAttributedString.DocumentType.html, .characterEncoding: String.Encoding.utf8.rawValue], documentAttributes: nil)
+    }
+    
+    var htmlToString: String {
+        return htmlToAttributedString?.string ?? ""
+    }
+    
 }
--- Cider/Views/MainViews/DetailedView.swift
+++ Cider/Views/MainViews/DetailedView.swift
@@ -200,14 +200,3 @@ struct DetailedView_Previews: PreviewProvider {
         DetailedView(detailedViewParams: DetailedViewParams(item: .mediaItem(MediaItem(data: [])), geometryMatching: stubId, originalSize: .zero))
     }
 }
-
-extension String {
-    var htmlToAttributedString: NSAttributedString? {
-        guard let data = data(using: .utf8) else { return nil }
-        return try? NSAttributedString(data: data, options: [.documentType: NSAttributedString.DocumentType.html, .characterEncoding: String.Encoding.utf8.rawValue], documentAttributes: nil)
-    }
-    
-    var htmlToString: String {
-        return htmlToAttributedString?.string ?? ""
-    }
-}
